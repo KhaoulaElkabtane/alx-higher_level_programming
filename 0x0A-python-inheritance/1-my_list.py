a38ac4fde1001fa91d65e4ab68d8202f67c43ff4
KhaoulaElkabtane <elkabtanekhaoula@gmail.com>
2023-10-09 15:42:16 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a class My list"""
+
+class MyList(list):
+    def __init__(self):
+        """Initializes a new instance of My list"""
+        super().__init__()
+    def print_sorted(self):
+        """ Prints the elements of the list in ascending order."""
+        print(sorted(self))
