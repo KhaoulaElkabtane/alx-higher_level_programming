657c712256c81e1594b6a7cc329cc7394c38a469
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-10-11 00:56:16 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class My list"""
+"""Defines a class My list."""
 
 class MyList(list):
-    """A subclass of list"""
+    """A subclass of list."""
     def print_sorted(self):
         """Prints the elements of the list in ascending order."""
         print(sorted(self))
