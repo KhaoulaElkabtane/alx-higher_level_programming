69e20688e9aaedc810b5381078bc8e58822d6606
KhaoulaElkabtane <elkabtanekhaoula@gmail.com>
2023-10-10 10:39:54 -0700

    3-main.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+"""Define a function, is_king_of_class,"""
+
+def is_kind_of_class(obj, a_class):
+    """Return true if is instance of, or is an instance of a class
+    that inherited from"""
+    return isinstance(obj, a_class)
