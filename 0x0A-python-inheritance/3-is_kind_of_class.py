b4dbcd49f5e336c08c91d756edd65c0c109a1a6f
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-10-11 01:01:46 +0100

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a function, is_king_of_class,"""
 
+
 def is_kind_of_class(obj, a_class):
     """Determines if an object is a subclass of a class."""
     if isinstance(obj, a_class):
